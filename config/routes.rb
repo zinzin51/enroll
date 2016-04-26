59927afe29d6e33772efe1486ad4ea5d5b3883dd
JHicks2112 <jlhicks4925@gmail.com>
2016-04-26 09:13:12 -0400

    changed layout so that SEP History is another page

--- config/routes.rb
+++ config/routes.rb
@@ -338,6 +338,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :sep_history, only: [:index]
+
   # Temporary for Generic Form Template
   match 'templates/form-template', to: 'welcome#form_template', via: [:get, :post]
 
