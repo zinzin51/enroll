12696b781354262ecf62231c243815666bfadbcb
JHicks2112 <jlhicks4925@gmail.com>
2016-05-27 18:14:18 -0400

    merged more conflicts

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -24,11 +24,24 @@ FactoryGirl.define do
   trait :hbx_staff do
     roles ["hbx_staff"]
 
-    after :create do |user, evaluator|
-      if user.person.present?
-      user.person.hbx_staff_role = FactoryGirl.build :hbx_staff_role
-      user.save
+    ######################
+    # this is our code #
+    ####################
+    after :create do |user|
+      FactoryGirl.create :person, :with_hbx_staff_role, :with_family, :user => user
       end
+      
+
+    ######################
+    # below is master code #
+    ####################
+
+    #after :create do |user, evaluator|
+     # if user.person.present?
+      #user.person.hbx_staff_role = FactoryGirl.build :hbx_staff_role
+      #user.save
+      #end
+    #end
   end
 
   trait :consumer do
