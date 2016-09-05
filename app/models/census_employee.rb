9dd782d2d5f89773e1e9e2390ffc3ce946ce6b57
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2016-09-05 23:02:56 +0000

    removed unneeded method

--- app/models/census_employee.rb
+++ app/models/census_employee.rb
@@ -356,15 +356,10 @@ class CensusEmployee < CensusMember
     active_benefit_group_assignment.present? && active_benefit_group_assignment.initialized?
   end
 
-  def active_or_renewal_benefit_group_assignment_for(plan_year)
-    benefit_group_ids = plan_year.benefit_groups.map(&:id)
-    [active_benefit_group_assignment, renewal_benefit_group_assignment].detect do |assignment|
-      benefit_group_ids.include?(assignment.try(:benefit_group_id))
-    end
-  end
-
   def has_active_health_coverage?(plan_year)
-    bg_assignment = active_or_renewal_benefit_group_assignment_for(plan_year)
+    benefit_group_ids = plan_year.benefit_groups.map(&:id)
+    bg_assignment = active_benefit_group_assignment if benefit_group_ids.include?(active_benefit_group_assignment.try(:benefit_group_id))
+    bg_assignment = renewal_benefit_group_assignment if benefit_group_ids.include?(renewal_benefit_group_assignment.try(:benefit_group_id))
     bg_assignment.present? && HbxEnrollment.find_shop_and_health_by_benefit_group_assignment(bg_assignment).present?
   end
 
