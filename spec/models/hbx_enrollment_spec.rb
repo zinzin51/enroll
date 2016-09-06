e5ddbf3e44bf18362e74356ace00d6d16d38ebaf
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2016-09-06 19:58:16 +0000

    fix for number of waived employees, rspec test of model

--- spec/models/hbx_enrollment_spec.rb
+++ spec/models/hbx_enrollment_spec.rb
@@ -1725,58 +1725,72 @@ end
 
 describe HbxEnrollment, type: :model, dbclean: :after_each do
    
-    let(:employer_profile)          { FactoryGirl.create(:employer_profile) }
-    let(:calender_year)             { TimeKeeper.date_of_record.year }
+    let!(:employer_profile_cafe)      { FactoryGirl.create(:employer_profile) }
+    let!(:employer_profile_salon)     { FactoryGirl.create(:employer_profile) }
+    let!(:calender_year)              { TimeKeeper.date_of_record.year }
 
-    let(:middle_of_prev_year) { Date.new(calender_year - 1, 6, 10) }
-  
-    let(:shop_family)       { FactoryGirl.create(:family, :with_primary_family_member) }
-    let(:plan_year_start_on) { Date.new(calender_year, 1, 1) }
-    let(:plan_year_end_on) { Date.new(calender_year, 12, 31) }
-    let(:open_enrollment_start_on) { Date.new(calender_year - 1, 12, 1) }
-    let(:open_enrollment_end_on) { Date.new(calender_year - 1, 12, 10) }
-    let(:effective_date)         { plan_year_start_on }
+    let!(:middle_of_prev_year)        { Date.new(calender_year - 1, 6, 10) }
+   
+    let!(:shop_family)                { FactoryGirl.create(:family, :with_primary_family_member) }
+    let!(:plan_year_start_on)         { Date.new(calender_year, 1, 1) }
+    let!(:plan_year_end_on)           { Date.new(calender_year, 12, 31) }
+    let!(:open_enrollment_start_on)   { Date.new(calender_year - 1, 12, 1) }
+    let!(:open_enrollment_end_on)     { Date.new(calender_year - 1, 12, 10) }
+    let!(:effective_date)             { plan_year_start_on }
+
+    ["cafe", "salon"].each do |id|  
+        employer_profile_id = "employer_profile_#{id}".to_sym
+        plan_year_id = "plan_year_#{id}".to_sym
+        let!(plan_year_id)                  { py = FactoryGirl.create(:plan_year,
+                                               start_on: plan_year_start_on,
+                                               end_on: plan_year_end_on,
+                                               open_enrollment_start_on: open_enrollment_start_on,
+                                               open_enrollment_end_on: open_enrollment_end_on,
+                                               employer_profile: send(employer_profile_id)
+                                             )
+
+                                             blue = FactoryGirl.build(:benefit_group, title: "blue collar", plan_year: py)
+                                             white = FactoryGirl.build(:benefit_group, title: "white collar", plan_year: py)
+                                             py.benefit_groups = [blue, white]
+                                             py.save
+                                             py.update_attributes({:aasm_state => 'published'})
+                                             py
+                                          }
+      end
+
+    
 
 
-    let!(:plan_year)                               { py = FactoryGirl.create(:plan_year,
-                                                      start_on: plan_year_start_on,
-                                                      end_on: plan_year_end_on,
-                                                      open_enrollment_start_on: open_enrollment_start_on,
-                                                      open_enrollment_end_on: open_enrollment_end_on,
-                                                      employer_profile: employer_profile
-                                                    )
-
-                                                    blue = FactoryGirl.build(:benefit_group, title: "blue collar", plan_year: py)
-                                                    white = FactoryGirl.build(:benefit_group, title: "white collar", plan_year: py)
-                                                    py.benefit_groups = [blue, white]
-                                                    py.save
-                                                    py.update_attributes({:aasm_state => 'published'})
-                                                    py
-                                                  }
-
-  
-    [{id: :barista, name: 'John'}, 
-     {id: :manager,  name: 'Grace'}, 
-     {id: :hairdresser,  name: 'Tatiana'}
-     ].each_with_index do |record, index|  
+    [{id: :barista, name: 'John', coverage_kind: "health", works_at: 'cafe', collar: "blue"}, 
+     {id: :manager,  name: 'Grace', coverage_kind: "health", works_at: 'cafe', collar: "white"}, 
+     {id: :janitor,  name: 'Bob', coverage_kind: "dental", works_at: 'cafe', collar: "blue"}, 
+     {id: :hairdresser,  name: 'Tatiana', coverage_kind: "health", works_at: 'salon', collar: "blue"}
+    ].each_with_index do |record, index|  
         id = record[:id]
         name = record[:name]
+        works_at = record[:works_at]
+        coverage_kind = record[:coverage_kind]
+        social_class = "#{record[:collar]} collar"
+        employer_profile_id = "employer_profile_#{works_at}".to_sym
+        plan_year_id = "plan_year_#{works_at}".to_sym
         census_employee_id = "census_employee_#{id}".to_sym
         employee_role_id = "employee_role_#{id}".to_sym
         benefit_group_assignment_id = "benefit_group_assignment_#{id}".to_sym
         shop_enrollment_id = "shop_enrollment_#{id}".to_sym
+        get_benefit_group = -> (year) { year.benefit_groups.detect {|bg| bg.title == social_class} }
 
         let!(id) { 
-            FactoryGirl.create(:person, first_name: name, last_name: 'Smith', dob: '1966-10-10'.to_date, ssn: "99966771#{index}") 
+            FactoryGirl.create(:person, first_name: name, last_name: 'Smith', 
+              dob: '1966-10-10'.to_date, ssn: "99966771#{index}") 
         }
     
         let!(census_employee_id) {
-           FactoryGirl.create(:census_employee, first_name: 'John', last_name: 'Smith', dob: '1966-10-10'.to_date, ssn: "99966770#{index}", created_at: middle_of_prev_year, updated_at: middle_of_prev_year, hired_on: middle_of_prev_year) 
+           FactoryGirl.create(:census_employee, first_name: name, last_name: 'Smith', dob: '1966-10-10'.to_date, ssn: "99966770#{index}", created_at: middle_of_prev_year, updated_at: middle_of_prev_year, hired_on: middle_of_prev_year) 
         }
     
         let!(employee_role_id) {
             send(record[:id]).employee_roles.create(
-              employer_profile: employer_profile,
+              employer_profile: send(employer_profile_id),
               hired_on: send(census_employee_id).hired_on,
               census_employee_id: send(census_employee_id).id
             )
@@ -1785,98 +1799,57 @@ describe HbxEnrollment, type: :model, dbclean: :after_each do
         let!(benefit_group_assignment_id) {
           BenefitGroupAssignment.create({
             census_employee: send(census_employee_id),
-            benefit_group: plan_year.benefit_groups.first,
+            benefit_group: get_benefit_group.call(send(plan_year_id)),
             start_on: plan_year_start_on
           })
         }
 
-
-        let!(shop_enrollment_id)   { FactoryGirl.create(:hbx_enrollment,
-                                                        household: shop_family.latest_household,
-                                                        coverage_kind: "health",
-                                                        effective_on: effective_date,
-                                                        enrollment_kind: "open_enrollment",
-                                                        kind: "employer_sponsored",
-                                                        submitted_at: effective_date - 10.days,
-                                                        benefit_group_id: plan_year.benefit_groups.first.id,
-                                                        employee_role_id: send(employee_role_id).id,
-                                                        benefit_group_assignment_id: send(benefit_group_assignment_id).id
-                                                      )
-          }
+        let!(shop_enrollment_id) { 
+                                    benefit_group = get_benefit_group.call(send(plan_year_id))
+                                    bga_id = send(benefit_group_assignment_id).id
+                                    FactoryGirl.create(:hbx_enrollment,
+                                                      household: shop_family.latest_household,
+                                                      coverage_kind: coverage_kind,
+                                                      effective_on: effective_date,
+                                                      enrollment_kind: "open_enrollment",
+                                                      kind: "employer_sponsored",
+                                                      submitted_at: effective_date - 10.days,
+                                                      benefit_group_id: benefit_group.id,
+                                                      employee_role_id: send(employee_role_id).id,
+                                                      benefit_group_assignment_id: bga_id
+                                                    )
+                                  }
 
           before do
-            allow(send(employee_role_id)).to receive(:benefit_group).and_return(plan_year.benefit_groups.first)
-            allow(send(census_employee_id)).to receive(:active_benefit_group_assignment).and_return(send(benefit_group_assignment_id))
+            benefit_group_assignment = send(benefit_group_assignment_id)
+            allow(send(employee_role_id)).to receive(:benefit_group).and_return(benefit_group_assignment.benefit_group)
+            allow(send(census_employee_id)).to receive(:active_benefit_group_assignment).and_return(benefit_group_assignment)
             allow(send(shop_enrollment_id)).to receive(:employee_role).and_return(send(employee_role_id))
           end
     end
-
-
-
-#      let(:employer_profile)                        { FactoryGirl.create(:employer_profile) }
-#     # let!(:census_employees) do
-#     #[1,2].collect do
-#     #  FactoryGirl.create(:census_employee, employer_profile: employer_profile)
-#     #end.sort_by(&:id)
-#     #end
-#      let(:plan_year_start_on)                      { TimeKeeper.date_of_record.next_month.end_of_month + 1.day }
-#      let(:plan_year_end_on)                        { (plan_year_start_on + 1.year) - 1.day }
-
-#      let(:benefit_group)                             { FactoryGirl.create(:benefit_group)}
-#      let!(:plan_year)                               { py = FactoryGirl.create(:plan_year,
-#                                                     start_on: plan_year_start_on,
-#                                                     end_on: plan_year_end_on,
-#                                                     open_enrollment_start_on: TimeKeeper.date_of_record,
-#                                                     open_enrollment_end_on: TimeKeeper.date_of_record + 5.days,
-#                                                     employer_profile: employer_profile,
-#                                                     benefit_groups: [benefit_group]
-#                                                       )
-#                                                       benefit_group.plan_year = py
-#                                                       benefit_group.save  
-#                                                       py.save
-#                                                       py.publish!
-#                                                       py
-#                                                     }
-#     let(:barista)                                   { FactoryGirl.create(:person, :with_employee_role) }
-#     let(:manager)                                   { FactoryGirl.create(:person, :with_employee_role) }
-#     let(:hairdresser)                               { FactoryGirl.create(:person, :with_employee_role) }
-#     let(:household)                                 { Household.new(:is_active => true) } 
-#     let(:family_member_barista)                     { FamilyMember.new(is_primary_applicant: true, 
-#                                                               is_consent_applicant: true, person: barista) }
-#     let(:family_member_manager)                     { FamilyMember.new(is_primary_applicant: true, 
-#                                                               is_consent_applicant: true, person: manager) }
-#     let(:family_member_hairdresser)                 { FamilyMember.new(is_primary_applicant: true, 
-#                                                           is_consent_applicant: true, person: hairdresser) }
-
-
-#     let(:family) { f = Family.new(:households => [household], :family_members => [family_member_barista, family_member_manager, family_member_hairdresser]) 
-#       f.save
-#       f
-#     }  
       
-      context "count_shop_and_health_enrolled_by_benefit_group_assignments" do
+    context "count_shop_and_health_enrolled_by_benefit_group_assignments" do
 
-        it "should count enrollment for two people in same family who works for same employer" do
-          #print "PY *******#{plan_year.inspect}******\n\n"
-          #print "BG *******#{plan_year.benefit_groups}******\n\n"
-          #print "ER *******#{employer_profile.inspect}\n\n"
-          #print "FM *******#{shop_family.inspect} \n\n"
-          #print "Barista  *****#{barista.inspect}\n\n\n\n"  
-
-          expect(barista.employee_roles.first.employer_profile.id).to eq (employer_profile.id)
+        it "should count enrollment for three people in the same family who work for the same employer, but one has only dental" do
+          
+          #check that our connections are as we expect
+          expect(barista.employee_roles.first.employer_profile.id).to eq (employer_profile_cafe.id)
           expect(barista.employee_roles.first.id).to eq (employee_role_barista.id)
           expect(barista.employee_roles.first.census_employee.id).to eq (census_employee_barista.id)
 
-          bgas = [benefit_group_assignment_barista, benefit_group_assignment_manager]  
-          expect(HbxEnrollment.count_shop_and_health_enrolled_by_benefit_group_assignments(bgas)).to be 2
-          #print "Manager #{manager.inspect}\n with census_employee_id #{manager.census_employee_id}, employer_profile_id #{manager.employer_profile_id} \n\n"  
-          #print "Hairdresser role  *****#{hairdresser.employee_role}*****\n\n"  
+          salon_benefit_groups = [benefit_group_assignment_hairdresser]  
+          cafe_benefit_groups = [benefit_group_assignment_barista, benefit_group_assignment_manager,
+            benefit_group_assignment_janitor] 
 
-        #   expect(HbxEnrollment.count_shop_and_health_enrolled_by_benefit_group_assignments().count).to be 
+          #there are three people working at the cafe, but only two have health insurance    
+          expect(HbxEnrollment.count_shop_and_health_enrolled_by_benefit_group_assignments(cafe_benefit_groups)).to be 2
+
+          #there is one person working at the salon    
+          expect(HbxEnrollment.count_shop_and_health_enrolled_by_benefit_group_assignments(salon_benefit_groups)).to be 1
+
+           
          end
 
-        it "should count enrollment for two people in different households in the same family" do
-        
-        end
+       # TODO it "should count enrollment for two people in different households in the same family" do
     end
 end
