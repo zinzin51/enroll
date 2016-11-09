5853a62ac8981b992a0d8e36e6ac40358e2c4e6a
Battula <vrbattula@outlook.com>
2016-11-09 16:55:39 -0500

    refs #11022 updates to scripts

--- script/load_organization_11022.rb
+++ script/load_organization_11022.rb
@@ -1,34 +1,104 @@
-fein ="464197120"
+
+fein ="521992631"
 folder_name = "tmp/dump_#{fein}"
+
+def files
+  fein ="521992631"
+  folder_name = "tmp/dump_#{fein}"
+  Dir.entries(folder_name)
+end
+
+def load_census_employee(file)
+  fein ="521992631"
+  folder_name = "tmp/dump_#{fein}"
+  ce_file = File.open(File.expand_path(folder_name+"/#{file}"),'rb')
+  ce = Marshal.load(ce_file)
+  ce_clone = ce.clone
+  ce_clone.ssn ="123456789"
+  ce_clone.save(:validate => false)
+end
+
+def load_person_and_families(file)
+  fein ="521992631"
+  folder_name = "tmp/dump_#{fein}"
+  person_file = File.open(File.expand_path(folder_name+"/#{file}"),'rb')
+  person = Marshal.load(person_file)
+  person_clone = person.clone
+  family_files = files.select { |f| f.match("dump_marshal_family_#{person.id}") }
+  family_files.each do |file|
+    family_file = File.open(File.expand_path(folder_name+"/#{file}"),'rb')
+    family = Marshal.load(family_file)
+    family_clone = family.clone
+    family_clone.primary_family_member.person_id = person_clone.id
+    family_clone.save(:validate => false)
+  end
+  person_clone.ssn = "123456789"
+  person_clone.save(:validate => false)
+end
+
+###
 begin
+  #load employer profile
   org_file = File.open(File.expand_path(folder_name+"/org"),'rb')
   org = Marshal.load(org_file)
-  org.save
-  emp_file = File.open(File.expand_path(folder_name+"/employer_profile"),'rb')
-  empl_profile = Marshal.load(emp_file)
-  empl_profile.save
+  org_clone = org.clone
+  org_clone.save
+  bg = org_clone.employer_profile.plan_years.last.benefit_groups.last
+  # emp_file = File.open(File.expand_path(folder_name+"/employer_profile"),'rb')
+  # empl_profile = Marshal.load(emp_file)
+  # empl_profile_clone = empl_profile.clone
+  # empl_profile_clone.save
 
+  # load broker agency profile
   broker_agency_file = File.open(File.expand_path(folder_name+"/broker_agency"),'rb')
   broker_agency = Marshal.load(broker_agency_file)
-  broker_agency.save
+  broker_agency_clone = broker_agency.clone
+  broker_agency_clone.save
+
+  #load reference health plan
+  plan_file = File.open(File.expand_path(folder_name+"/plan"),'rb')
+  plan = Marshal.load(plan_file)
+  plan_clone = plan.clone
+  plan_clone.save
+  bg.elected_plans << plan_clone
+  bg.reference_plan_id = plan_clone.id
+  bg.save(:validate => false)
+
+  #load dental reference plan
+  dental_plan_file = File.open(File.expand_path(folder_name+"/dental_plan"),'rb') rescue nil
+  if !dental_plan_file.nil?
+    dental_plan = Marshal.load(dental_plan_file)
+    dental_plan_clone = dental_plan.clone
+    dental_plan_clone.save
+    bg.elected_plans << dental_plan_clone
+    bg.dental_reference_plan_id = dental_plan_clone.id
+    bg.save(:validate => false)
+  end
+
+  br_profile = org.employer_profile.broker_agency_profile
   br_role_file = File.open(File.expand_path(folder_name+"/br_role"),'rb')
   brkr_role = Marshal.load(br_role_file)
-  brkr_role.save
+  brkr_role_clone = brkr_role.clone
+  # brkr_role_clone.save
+  br_profile.primary_broker_role_id = brkr_role_clone.id
+  br_profile.save
+
   person_broker_file = File.open(File.expand_path(folder_name+"/person_broker"),'rb')
   person_brkr = Marshal.load(person_broker_file)
   person_brkr_clone = person_brkr.clone
   person_brkr_clone.ssn ="987654321"
   person_brkr_clone.save(:validate => false)
 
-  files = Dir.entries(folder_name)
+
+
   files.each do |file|
-    next unless file.match("dump_marshal_census_employee")
-    ce_file = File.open(File.expand_path(folder_name+"/#{file}"),'rb')
-    ce = Marshal.load(ce_file)
-    ce_clone = ce.clone 
-    ce_clone.ssn ="123456789"
-    ce_clone.save(:validate => false)
+    case file
+    when /dump_marshal_census_employee/
+      load_census_employee(file)
+    when /dump_marshal_person/
+      load_person_and_families(file)
+    end
   end
 rescue Exception => e
-  puts "#{e} #{e.backtrace}"
+  puts "Error while loading census_employee #{e} #{e.backtrace}"
 end
--- script/organization_dump_11022.rb
+++ script/organization_dump_11022.rb
@@ -1,4 +1,5 @@
-fein ="464197120"
+
+fein ="521992631"
 folder_name = "tmp/dump_#{fein}"
 begin
   FileUtils.mkdir_p(folder_name)
@@ -6,11 +7,35 @@ begin
   File.open(File.expand_path(folder_name+"/org"),'wb'){|f| f.write(Marshal.dump(org))}
   emp_profile = org.employer_profile
   File.open(File.expand_path(folder_name+"/employer_profile"),'wb'){|f| f.write(Marshal.dump(emp_profile))}
+  bg = org.employer_profile.plan_years.last.benefit_groups.last
+  plan = bg.reference_plan
+  File.open(File.expand_path(folder_name+"/plan"),'wb'){|f| f.write(Marshal.dump(plan))}
+  if !bg.dental_reference_plan.nil?
+    plan = bg.dental_reference_plan
+    File.open(File.expand_path(folder_name+"/dental_plan"),'wb'){|f| f.write(Marshal.dump(plan))}
+  end
   broker_agency = org.employer_profile.broker_agency_profile
-  File.open(File.expand_path(folder_name+"/broker_agency"),'wb'){|f| f.write(Marshal.dump(broker_agency))}
+  if !broker_agency.nil?
+    File.open(File.expand_path(folder_name+"/broker_agency"),'wb'){|f| f.write(Marshal.dump(broker_agency.organization))}
+    br_role = broker_agency.primary_broker_role
+    File.open(File.expand_path(folder_name+"/br_role"),'wb'){|f| f.write(Marshal.dump(br_role))}
+    person_broker = broker_agency.primary_broker_role.person
+    File.open(File.expand_path(folder_name+"/person_broker"),'wb'){|f| f.write(Marshal.dump(person_broker))}
+  end
   org.employer_profile.census_employees.each do |ce|
-    file_path = "tmp/dump_#{fein}/dump_marshal_census_employee_#{ce.id}"
-    File.open(File.expand_path(file_path),'wb'){|f| f.write(Marshal.dump(ce))}
+    personid = ce.employee_role.person.id
+    file_path_ce = "tmp/dump_#{fein}/dump_marshal_census_employee_#{personid}"
+    File.open(File.expand_path(file_path_ce),'wb'){|f| f.write(Marshal.dump(ce))}
+    file_path_ee = "tmp/dump_#{fein}/dump_marshal_employee_role_#{personid}"
+    File.open(File.expand_path(file_path_ee),'wb'){|f| f.write(Marshal.dump(ce.employee_role))}
+    file_path_person = "tmp/dump_#{fein}/dump_marshal_person_#{personid}"
+    File.open(File.expand_path(file_path_person),'wb'){|f| f.write(Marshal.dump(Person.includes(:user).find(personid)))}
+    # file_path_user = "tmp/dump_#{fein}/dump_marshal_user_#{ce.id}"
+    # File.open(File.expand_path(file_path_user),'wb'){|f| f.write(Marshal.dump(ce.employee_role.person.user))}
+    file_path_family = "tmp/dump_#{fein}/dump_marshal_family_#{personid}"
+    File.open(File.expand_path(file_path_family),'wb'){|f| f.write(Marshal.dump(ce.employee_role.person.families.first))}
+    # file_path_primary = "tmp/dump_#{fein}/dump_marshal_primary_#{ce.id}"
+    # File.open(File.expand_path(file_path_primary),'wb'){|f| f.write(Marshal.dump(ce.employee_role.person.families.first.primary_family_member))}
 end
 rescue Exception => e
   puts "#{e} #{e.backtrace}"
