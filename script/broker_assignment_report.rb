13daf42dbdd2d3703c8e5bf5116899764f5ef1e7
Battula <vrbattula@outlook.com>
2017-01-17 12:37:22 -0500

    refs #13162 Add ER FEIN to the report

--- script/broker_assignment_report.rb
+++ script/broker_assignment_report.rb
@@ -6,15 +6,16 @@
   org_count = Organization.count
   field_names  = %w(
              Group_name
-             FEIN
+             Employer_FEIN
              Broker_Name
              Broker_NPN
              Broker_Assignment_Start
              Broker_Assignment_end
              )
 
-  file_name = "#{Rails.root}/brokers_assignment_report_#{TimeKeeper.date_of_record.strftime("%m_%d_%Y")}.csv"
-
+  processed_count = 0
+  file_name = "#{Rails.root}/broker_assignment_report_#{TimeKeeper.date_of_record.strftime("%m_%d_%Y")}.csv"
+  date_range = Date.new(2016,1,1)..TimeKeeper.date_of_record
   CSV.open(file_name, "w", force_quotes: true) do |csv|
     
     csv << field_names
@@ -25,15 +26,16 @@
           next if  (ba_account.nil? || ba_account.broker_agency_profile.nil?) || ba_account.created_at >= Date.new(2016,1,1) ||  ba_account.broker_agency_profile.market_kind == "individual"
           csv << [
             employer.legal_name,
-            ba_account.broker_agency_profile.fein,
+            employer.fein,
             ba_account.broker_agency_profile.primary_broker_role.person.full_name,
             ba_account.broker_agency_profile.primary_broker_role.npn,
             ba_account.start_on,
             ba_account.end_on
           ]
         end
+        processed_count += 1
       end
       offset = offset + batch_size
     end
-    puts "For period #{date_range.first} - #{date_range.last}, #{processed_count} Brokers to output file: #{file_name}" unless Rails.env.test?
+    puts "For period #{date_range.first} - #{date_range.last}, #{processed_count} brokers to output file: #{file_name}" unless Rails.env.test?
   end
