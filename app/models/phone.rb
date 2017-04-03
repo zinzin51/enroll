8f818cb888cf2cce745fe5b217aa85ed26b5834e
Jayanth <jayanth.rordev@gmail.com>
2017-04-03 16:17:33 -0400

    refs#11251 Updating office phone kind enumeration

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -7,7 +7,7 @@ class Phone
   embedded_in :census_member, class_name: "CensusMember"
 
   KINDS = ["home", "work", "mobile", "main", "fax"]
-  OFFICE_KINDS = ["phone main"]
+  OFFICE_KINDS = ["main", "direct"]
 
   field :kind, type: String
   field :country_code, type: String, default: ""
