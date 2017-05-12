12c4ecc4496a039d48e197f6c570cecb0dddc2df
Raghuram <raghuramg83@gmail.com>
2017-05-12 14:30:07 -0400

    Refs #14176 Added migration to fix renewal plans

--- app/data_migrations/correct_invalid_csr_variant_renewals.rb
+++ app/data_migrations/correct_invalid_csr_variant_renewals.rb
@@ -5,28 +5,46 @@ class CorrectInvalidCsrVariantRenewals < MongoidMigrationTask
   def migrate
     enrollment_group_ids = ENV['hbx_ids'].split(',')
     renewal_begin_date = Date.strptime(ENV['renewal_date'], "%m/%d/%Y")
+    valid_statuses = %w(coverage_selected transmitted_to_carrier coverage_enrolled coverage_expired enrolled_contingent unverified)
 
     enrollment_group_ids.each do |hbx_id|
       enrollment = HbxEnrollment.by_hbx_id(hbx_id.to_s).first
+      if enrollment.blank?
+        puts "Enrollment missing for EG id #{hbx_id}."
+        next
+      end
 
-      if enrollment.present?
-        family = enrollment.family
-        enrollments = family.active_household.hbx_enrollments.where({
-          effective_on: renewal_begin_date, 
+      if enrollment.workflow_state_transitions.where(to_state: 'auto_renewing').present?
+        coverage_start = enrollment.effective_on.prev_year
+
+        enrollments = enrollment.household.hbx_enrollments.where({
+          :effective_on.gte => coverage_start,
+          :effective_on.lt => enrollment.effective_on,
           kind: enrollment.kind, 
           coverage_kind: enrollment.coverage_kind
-        }).enrolled_and_renewing
+        }).where(:aasm_state.in => valid_statuses)
+      else
+        puts "Enrollment Group #{hbx_id} not a passive renewal."
+      end
 
-        passive_renewal = enrollments.detect{|e| e.workflow_state_transitions.where(to_state: 'auto_renewing').present?}
+      if enrollments.present?
+        base_enrollment = enrollments.first
 
-        if passive_renewal.present?
+        if has_catastrophic_plan?(base_enrollment)
+          renewal_plan = base_enrollment.plan.cat_age_off_renewal_plan
 
-        else
-          puts "Passive Renewal for eg Id #{hbx_id} missing."
+          if enrollment.plan.hios_id != renewal_plan.hios_id
+            enrollment.update(plan_id: renewal_plan.id)
+            puts "EG #{hbx_id} is renewed into #{enrollment.plan.hios_id}...but the correct plan is #{renewal_plan.hios_id}."
+          end
         end
       else
-        puts "Enrollment for eg Id #{hbx_id} missing."
+        puts "Previous Year Enrollment missing for EG Id #{hbx_id}."
       end
     end
+  end
+
+  def has_catastrophic_plan?(enrollment)
+    enrollment.plan.metal_level == 'catastrophic'       
   end
 end
--- lib/tasks/migrations/correct_invalid_csr_variant_renewals.rake
+++ lib/tasks/migrations/correct_invalid_csr_variant_renewals.rake
@@ -3,5 +3,5 @@ require File.join(Rails.root, "app", "data_migrations", "correct_invalid_csr_var
 # RAILS_ENV=production bundle exec rake migrations:correct_invalid_csr_variant_renewals  hbx_ids="609882,609228,609306", renewal_date = "1/1/2017"
 namespace :migrations do
   desc "correct_invalid_csr_variant_renewals"
-  MergeEeAndErAccounts.define_task :correct_invalid_csr_variant_renewals => :environment
+  CorrectInvalidCsrVariantRenewals.define_task :correct_invalid_csr_variant_renewals => :environment
 end
