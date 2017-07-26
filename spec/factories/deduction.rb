c68772a0b8eb6d36d9ddb2b3377857ca5d6fadc5
sudhirreddysama <sudhirsama586@gmail.com>
2017-07-26 14:59:35 -0400

    Testcases for Deduction_spec

---
+++ spec/factories/deduction.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory(:deduction, :class => FinancialAssistance::Deduction) do
+    association :applicant
+
+    title 'Test'
+    amount 10
+    frequency_kind 'monthly'
+    start_on Date.today.beginning_of_month
+    end_on Date.today.end_of_month
+  end
+end
