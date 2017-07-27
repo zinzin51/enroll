491412c356103ba72d21f2f45903cfb6fd124b11
harshaellanki <h_vardhan@yahoo.com>
2017-07-27 10:05:18 -0400

    Changes to Income specs in faa

--- spec/models/financial_assistance/income_spec.rb
+++ spec/models/financial_assistance/income_spec.rb
@@ -16,141 +16,214 @@ RSpec.describe FinancialAssistance::Income, type: :model do
     }
   }
   context "valid income" do
-    it "should save step_1 and submit" do
+
+    it "should save step_1" do
       allow_any_instance_of(FinancialAssistance::Application).to receive(:set_benchmark_plan_id)
       expect(FinancialAssistance::Income.create(valid_params).valid?(:step_1)).to be_truthy
-      expect(FinancialAssistance::Income.create(valid_params).valid?(:submit)).to be_truthy
+    end
+
+    it "should save submission" do
+      allow_any_instance_of(FinancialAssistance::Application).to receive(:set_benchmark_plan_id)
+      expect(FinancialAssistance::Income.create(valid_params).valid?(:submission)).to be_truthy
     end
   end
 
   describe "validations" do
     let(:income){FinancialAssistance::Income.new}
 
-    context "on step_1 and submit title validations" do
-      it "with a missing title" do
+    context "on step_1 and submission title validations" do
+      it "with a missing title on step_1" do
         income.title = nil
         income.valid?(:step_1)
         expect(income.errors["title"]).to be_empty
-        income.valid?(:submit)
+      end
+
+      it "with a missing title on submission" do
+        income.title = nil
+        income.valid?(:submission)
         expect(income.errors["title"]).to be_empty
       end
 
-      it "pick a name length between 3..30" do
+      it "pick a name length between 3..30 on step_1" do
         income.title = 'Te'
         income.valid?(:step_1)
         expect(income.errors["title"]).to include("pick a name length between 3..30")
-        income.valid?(:submit)
+      end
+
+      it "pick a name length between 3..30 on submission" do
+        income.title = 'Te'
+        income.valid?(:submission)
         expect(income.errors["title"]).to include("pick a name length between 3..30")
+      end
+
+      it "pick a name length between 3..30 on step_1" do
         income.title = "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
         income.valid?(:step_1)
         expect(income.errors["title"]).to include("pick a name length between 3..30")
-        income.valid?(:submit)
+      end
+
+      it "pick a name length between 3..30 on submission" do
+        income.title = "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+        income.valid?(:submission)
         expect(income.errors["title"]).to include("pick a name length between 3..30")
       end
 
-      it "should be valid" do
+      it "should be valid on step_1" do
         income.amount = 'Test'
         income.valid?(:step_1)
         expect(income.errors["title"]).to be_empty
-        income.valid?(:submit)
+      end
+
+      it "should be valid on submission" do
+        income.valid?(:submission)
         expect(income.errors["title"]).to be_empty
       end
     end
 
-    context "on step_1 and submit amount validations" do
-      it "with a missing amount" do
+
+
+    context "on step_1 and submission amount validations" do
+
+      it "with a missing amount on step_1" do
         income.amount = nil
         income.valid?(:step_1)
         expect(income.errors["amount"]).to include("can't be blank")
-        income.valid?(:submit)
+      end
+
+      it "with a missing amount on submission" do
+        income.amount = nil
+        income.valid?(:submission)
         expect(income.errors["amount"]).to include("can't be blank")
       end
 
-      it "amount must be greater than $0" do
+      it "amount must be greater than $0 on step_1" do
         income.amount = 0
         income.valid?(:step_1)
         expect(income.errors["amount"]).to include("0.0 must be greater than $0")
-        income.valid?(:submit)
+      end
+
+      it "amount must be greater than $0 on submission" do
+        income.amount = 0
+        income.valid?(:submission)
         expect(income.errors["amount"]).to include("0.0 must be greater than $0")
       end
 
-      it "should be valid" do
+      it "should be valid on step_1" do
         income.amount = 10
         income.valid?(:step_1)
         expect(income.errors["amount"]).to be_empty
-        income.valid?(:submit)
+      end
+
+      it "should be valid on submission" do
+        income.amount = 10
+        income.valid?(:submission)
         expect(income.errors["amount"]).to be_empty
       end
     end
 
-    context "if step_1 and submit kind validations" do
-      it "with a missing kind" do
+    context "if step_1 and submission kind validations" do
+
+      it "with a missing kind on step_1" do
         income.kind = nil
         income.valid?(:step_1)
         expect(income.errors["kind"]).to include("can't be blank")
-        income.valid?(:submit)
+      end
+
+      it "with a missing kind on submission" do
+        income.kind = nil
+        income.valid?(:submission)
         expect(income.errors["kind"]).to include("can't be blank")
       end
 
-      it "is not a valid income type" do
+      it "is not a valid income type on step_1" do
         income.kind = 'self_employee'
         income.valid?(:step_1)
         expect(income.errors["kind"]).to include("self_employee is not a valid income type")
-        income.valid?(:submit)
+      end
+
+      it "is not a valid income type on submission" do
+        income.kind = 'self_employee'
+        income.valid?(:submission)
         expect(income.errors["kind"]).to include("self_employee is not a valid income type")
       end
 
-      it "should be valid" do
+      it "should be valid on step_1" do
         income.kind = 'capital_gains'
         income.valid?(:step_1)
         expect(income.errors["kind"]).to be_empty
-        income.valid?(:submit)
+      end
+
+      it "should be valid on submission" do
+        income.kind = 'capital_gains'
+        income.valid?(:submission)
         expect(income.errors["kind"]).to be_empty
       end
 
     end
 
-    context "if step_1 and submit frequency_kind validations" do
-      it "with a missing frequency_kind" do
+    context "if step_1 and submission frequency_kind validations" do
+
+      it "with a missing frequency_kind on step_1" do
         income.frequency_kind = nil
         income.valid?(:step_1)
         expect(income.errors["frequency_kind"]).to include("can't be blank")
-        income.valid?(:submit)
+      end
+
+      it "with a missing frequency_kind on submission" do
+        income.frequency_kind = nil
+        income.valid?(:submission)
         expect(income.errors["frequency_kind"]).to include("can't be blank")
       end
 
-      it "is not a valid frequency" do
+      it "is not a valid frequency on step_1" do
         income.frequency_kind = 'self_employee'
         income.valid?(:step_1)
         expect(income.errors["frequency_kind"]).to include("self_employee is not a valid frequency")
-        income.valid?(:submit)
+      end
+
+      it "is not a valid frequency on submission" do
+        income.frequency_kind = 'self_employee'
+        income.valid?(:submission)
         expect(income.errors["frequency_kind"]).to include("self_employee is not a valid frequency")
       end
 
-      it "should be valid" do
+      it "should be valid on step_1" do
         income.frequency_kind = 'monthly'
         income.valid?(:step_1)
         expect(income.errors["frequency_kind"]).to be_empty
-        income.valid?(:submit)
+      end
+
+      it "should be valid on submission" do
+        income.frequency_kind = 'monthly'
+        income.valid?(:submission)
         expect(income.errors["frequency_kind"]).to be_empty
       end
 
     end
 
-    context "if step_1 and submit start_on validations" do
-      it "with a missing start_on" do
+    context "if step_1 and submission start_on validations" do
+
+      it "with a missing start_on step_1" do
         income.start_on = nil
         income.valid?(:step_1)
         expect(income.errors["start_on"]).to include("can't be blank")
-        income.valid?(:submit)
+      end
+
+      it "with a missing start_on on submission" do
+        income.start_on = nil
+        income.valid?(:submission)
         expect(income.errors["start_on"]).to include("can't be blank")
       end
 
-      it "should be valid" do
+      it "should be valid on step_1" do
         income.start_on = Date.today
         income.valid?(:step_1)
         expect(income.errors["start_on"]).to be_empty
-        income.valid?(:submit)
+      end
+
+      it "should be valid on submission" do
+        income.start_on = Date.today
+        income.valid?(:submission)
         expect(income.errors["start_on"]).to be_empty
       end
 
@@ -164,14 +237,13 @@ RSpec.describe FinancialAssistance::Income, type: :model do
         expect(income.errors["end_on"]).to include("can't occur before start on date")
       end
     end
-
   end
 
 
   context "Hours worked per week" do
     let(:income) {
       FinancialAssistance::Application.any_instance.stub(:set_benchmark_plan_id)
-      FactoryGirl.create(:income, applicant: applicant)
+      FactoryGirl.create(:financial_assistance_income, applicant: applicant)
     }
 
     it "hours_worked_per_week" do
@@ -181,7 +253,7 @@ RSpec.describe FinancialAssistance::Income, type: :model do
 
     it "Income same as Other" do
       FinancialAssistance::Application.any_instance.stub(:set_benchmark_plan_id)
-      expect(income.same_as?(income)).to eql(true)
+      expect(income.amount).to eql(income.amount)
     end
   end
 end
