a3077afb99cc7c4534f33e42b70572482b604f4c
sudhirreddysama <sudhirsama586@gmail.com>
2017-08-16 16:00:15 -0400

    added trigger for new hire enrollment

--- app/controllers/insured/plan_shoppings_controller.rb
+++ app/controllers/insured/plan_shoppings_controller.rb
@@ -56,11 +56,9 @@ class Insured::PlanShoppingsController < ApplicationController
 
     @change_plan = params[:change_plan].present? ? params[:change_plan] : ''
     @enrollment_kind = params[:enrollment_kind].present? ? params[:enrollment_kind] : ''
-
     @census_employee = @enrollment.census_employee
-    if (@change_plan.present? || @enrollment_kind.present?) && @census_employee.present?
-      employee_mid_year_plan_change_non_congressional(@census_employee)
-    end
+    binding.pry
+    employee_mid_year_plan_change_non_congressional(@census_employee) if  @census_employee.present?
     send_receipt_emails if @person.emails.first
   end
 
@@ -116,6 +114,8 @@ class Insured::PlanShoppingsController < ApplicationController
     if hbx_enrollment.may_waive_coverage? and waiver_reason.present? and hbx_enrollment.valid?
       hbx_enrollment.waive_coverage_by_benefit_group_assignment(waiver_reason)
       redirect_to print_waiver_insured_plan_shopping_path(hbx_enrollment), notice: "Waive Coverage Successful"
+      @census_employee = hbx_enrollment.census_employee
+      employee_mid_year_plan_change_non_congressional(@census_employee) if @census_employee.present?
     else
       redirect_to new_insured_group_selection_path(person_id: @person.id, change_plan: 'change_plan', hbx_enrollment_id: hbx_enrollment.id), alert: "Waive Coverage Failed"
     end
@@ -136,6 +136,8 @@ class Insured::PlanShoppingsController < ApplicationController
       hbx_enrollment.terminate_reason = params[:terminate_reason] if params[:terminate_reason].present?
       hbx_enrollment.schedule_coverage_termination!(@person.primary_family.terminate_date_for_shop_by_enrollment(hbx_enrollment))
       hbx_enrollment.update_renewal_coverage
+      @census_employee = hbx_enrollment.census_employee
+      employee_mid_year_plan_change_non_congressional(@census_employee) if @census_employee.present?
       
       redirect_to family_account_path
     else
